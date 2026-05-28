7b90f6120344e803e2f65390f39bec64cd8705d8
Muhammad mujahid <mohamed.mojahead@gmail.com>
2026-05-29 01:43:34 +0300

    add space to view reset of card data

--- src/pages/TensesCards.vue
+++ src/pages/TensesCards.vue
@@ -1,7 +1,7 @@
 <template>
   <PageHeader description="ملخص بسيط لكل زمن من حيث التكوين والاستخدام مع مثال للتوضيح" title="الأزمنة في الإنجليزية" />
   <div
-    class="p-3 max-w-7xl mx-auto grid grid-cols-1 gap-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2"
+    class="p-6 md:px-3 max-w-7xl mx-auto grid grid-cols-1 gap-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2"
   >
     <TenseCard v-for="tense in tenses" :key="tense.tenseName" :tense="tense" />
   </div>
