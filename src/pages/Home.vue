6585a6f67612ce97ff3dbcad2934c1469c7ca518
Muhammad mujahid <mohamed.mojahead@gmail.com>
2026-05-29 01:48:41 +0300

    fix home page color

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,7 +4,7 @@
       
       <div class="mx-auto max-w-2xl">
         <div class="text-center">
-          <h1 class="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">ملخص اللي بتعلمه في الإنجليزي</h1>
+          <h1 class="text-5xl font-semibold tracking-tight text-balance text-(--accent) sm:text-7xl">ملخص اللي بتعلمه في الإنجليزي</h1>
           <p class="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">أي حاجة بتعلمها في قواعد الإنجليزي بحاول أضيفها هنا كـUI لسببين الأول إني اراجعها بسهولة من الفون او اللاب والتاني اراجع على الحاجات اللي متعلمها في البرمجة وممكن اكون نسيتها بسبب الجيش</p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <a href="/tenses-cards" class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">كروت الأزمنة</a>
